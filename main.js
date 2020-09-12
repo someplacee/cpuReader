636b2cd6402c890b8615e337355c72387c46e57a
Christian <phonixlp112@gmail.com>
2020-09-12 13:49:28 +0200

    Update-Service

---
+++ main.js
@@ -0,0 +1,61 @@
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const os = require('os-utils');
+const { autoUpdater } = require('electron-updater');
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+
+  setInterval(() => {
+    os.cpuUsage(function(v){
+      mainWindow.webContents.send('cpu',v*100);
+      mainWindow.webContents.send('mem',os.freememPercentage()*100);
+      mainWindow.webContents.send('total-mem',os.totalmem()/1024);
+    });
+  },1000);
+
+  autoUpdater.checkForUpdatesAndNotify();
+}
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update_available');
+  });
+  autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update_downloaded');
+  });
+
+  ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
+  });
